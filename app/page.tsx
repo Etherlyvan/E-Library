6bdf129d6552fb17214da2b04f525f27beaa452c
Etherlyvan <jovanku1@gmail.com>
2024-11-16 17:05:36 +0700

    5

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
-// /app/page.tsx
-"use client"
-import BookCard from '@/components/book-card'
-import { books } from '@/constants/mockData'
-import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>ALL BOOKS</h1>
-      <div className={styles.containerStyle}>
-        <ul className={styles.ulGroupStyle}>
-          {books.map((book, i) => (
-            <li 
-              key={i}>
-              <a href={`/book/${book.id}`} style={{ textDecoration: 'none' }}>
-                <BookCard title={book.title} coverImage={book.image} description={book.description} />
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </main>
+    <div className="max-w-screen-xl mx-auto py-6 p-4">
+      <h1 className="text-2xl font-bold">Home Page</h1>
+    </div>
+    
   );
 }
