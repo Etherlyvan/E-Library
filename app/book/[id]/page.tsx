af66567d54a29cead1df6b24eca15b8568e06c86
Etherlyvan <jovanku1@gmail.com>
2024-11-16 17:00:12 +0700

    4

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,40 +1,16 @@
+import {auth} from "@/auth";
 
-"use client";
-import { useState } from 'react';
-import { books } from "@/constants/mockData";
-import { useParams } from "next/navigation";
-import styles from './book.module.css';
-
-export default function BookPage() {
-  const { id } = useParams();
-  const selectedBook = books.find(book => id === String(book.id));
-
-  // Inisialisasi content langsung dari localStorage jika buku ditemukan
-  const initialContent = selectedBook ? localStorage.getItem(`bookContent${selectedBook.id}`) || '' : '';
-  const [content, setContent] = useState(initialContent);
-
-  if (!selectedBook) return <p>Book not found</p>;
-
-  const notify = () => alert("Your changes have been saved!");
-
-  const handleSave = () => {
-    if (selectedBook) {
-      localStorage.setItem(`bookContent${selectedBook.id}`, content);
-      notify();
-    }
-  };
-
-  return (
-    <div className={styles.bookContainer}>
-      <aside>
-        <h1>{selectedBook.title}</h1>
-        <span>By {selectedBook.author}</span>
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          onBlur={handleSave}
-        />
-      </aside>
+const Dashboard = async () => {
+    const session = await auth();
+    return (
+        <div className="max-w-screen-xl mx-auto py-6 p-4">
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        <h2 className="text-xl">
+            Welcome Back: <span className="font-bold">{session?.user?.name}</span>
+        </h2>
+        <p>{JSON.stringify(session)}</p>
     </div>
-  );
-}
+    );
+};
+
+export default Dashboard;
